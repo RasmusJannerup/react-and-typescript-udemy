df159754dce0d64935e9769b00db2d6e7f0c974d
RasmusJannerup <93704010+RasmusJannerup@users.noreply.github.com>
2024-07-18 13:36:46 +0200

    chore: Add Vite configuration files and update project structure

--- data-fetching/src/App.tsx
+++ data-fetching/src/App.tsx
@@ -1,6 +1,7 @@
 import { ReactNode, useEffect, useState } from "react";
 import fetchingImg from './assets/data-fetching.png';
 import BlogPosts, { BlogPost } from "./components/BlogPosts";
+import ErrorMessage from "./components/ErrorMessage";
 import { get } from "./util/http";
 
 type RawDataBlogPost = {
@@ -12,18 +13,28 @@ type RawDataBlogPost = {
 
 function App() {
   const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
-
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState<string>();
 
   useEffect(() => {
     async function fetchPosts() {
-      const posts = (await get("https://jsonplaceholder.typicode.com/posts")) as RawDataBlogPost[];
-      setFetchedPosts(posts.map(rawPost => {
-        return {
-          id: rawPost.id,
-          title: rawPost.title,
-          text: rawPost.body
+      setIsFetching(true);
+      try {
+        const posts = (await get("https://jsonplaceholder.typicode.com/posts")) as RawDataBlogPost[];
+        setFetchedPosts(posts.map(rawPost => {
+          return {
+            id: rawPost.id,
+            title: rawPost.title,
+            text: rawPost.body
+          }
+        }));
+
+      } catch (error) {
+        if (error instanceof Error) {
+          setError(error.message);
         }
-      }));
+      }
+      setIsFetching(false);
     }
 
     fetchPosts();
@@ -31,8 +42,16 @@ function App() {
   }, []);
 
   let content: ReactNode;
+
+  if (error) {
+    content = <ErrorMessage text={error} />;
+  }
   if (fetchedPosts) {
     content = <BlogPosts posts={fetchedPosts} />;
+  }
+  if (isFetching) {
+    content = <p id='loading-fallback'>Loading...</p>;
+
   }
 
   return <main>
